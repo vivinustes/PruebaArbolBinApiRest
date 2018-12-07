71b1327f4557817623c8cbe0eb6dc85056c7db80
evelasquez1 <evelasquez@vertical.codaltec.com>
2018-12-07 00:43:40 -0500

    Agregar archivos de proyecto.

---
+++ PruebaArbolBinApiRest/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace PruebaArbolBinApiRest.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ PruebaArbolBinApiRest/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PruebaArbolBinApiRest.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
